406b73c873c0ddebe36cb75836aaa8dd7b28dd23
Lore Zumeta Olaskoaga <lorezumeta@gmail.com>
2023-09-18 18:22:40 +0200

    data updated

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,18 +8,19 @@ raw_df_injuries <- readRDS("../Injury-Scraping_data/injuries_df.Rds")
 years2select <- c(2017, 2018)
 teams2select <- c("fc-liverpool")
 
-raw_df_exposures <- raw_df_exposures %>%
+raw_df_exposures <- raw_df_exposures  |>
   filter(club_name %in% teams2select,
          year %in% years2select,
-         matches_played > 5) %>%
-  droplevels() %>%
-  mutate(place = iconv(place, from = "UTF-8", to = "ASCII")) %>%
+         matches_played > 5) |>
+  droplevels() |>
+  # mutate(place = iconv(place, from = "UTF-8", to = "ASCII")) |>
   select(player_name, player_id, season, year, matches_played, minutes_played,
-         liga, club_name, club_id, age, place, position, foot, goals, assists, reds)
-raw_df_injuries <- raw_df_injuries %>%
-  inner_join(raw_df_exposures %>% select(player_id, year), by = c("player_id" = "player_id",
-                                                                  "year" = "year")) %>%
-  droplevels() %>%
+         liga, club_name, club_id, age, height, place, citizenship, position,
+         foot, goals, assists, yellows, reds)
+raw_df_injuries <- raw_df_injuries |>
+  inner_join(raw_df_exposures |> select(player_id, year), by = c("player_id" = "player_id",
+                                                                  "year" = "year")) |>
+  droplevels() |>
   select(player_name, player_id, season, from, until, days_lost,
          games_lost, injury, injury_acl, injury_type, injury_severity)
 
@@ -49,25 +50,25 @@ years2select <- c(2017, 2018)
 # teams2select <- c("fc-bayern-munchen", "fc-liverpool", "fc-barcelona", "real-madrid")
 teams2select <- c("fc-bayern-munchen", "fc-liverpool")
 
-raw_df_exposures <- raw_df_exposures %>%
+raw_df_exposures <- raw_df_exposures |>
   filter(club_name %in% teams2select,
          year %in% years2select,
-         matches_played > 5) %>%
-  droplevels() %>%
-  mutate(place = iconv(place, from = "UTF-8", to = "ASCII")) %>%
+         matches_played > 5) |>
+  droplevels() |>
+  mutate(place = iconv(place, from = "UTF-8", to = "ASCII")) |>
   select(player_name, player_id, season, year, matches_played, minutes_played,
          liga, club_name, club_id, age, place, position, foot, goals, assists, reds)
-raw_df_injuries <- raw_df_injuries %>%
-  inner_join(raw_df_exposures %>% select(player_id, year), by = c("player_id" = "player_id",
-                                                                  "year" = "year")) %>%
-  droplevels() %>%
+raw_df_injuries <- raw_df_injuries |>
+  inner_join(raw_df_exposures |> select(player_id, year), by = c("player_id" = "player_id",
+                                                                  "year" = "year")) |>
+  droplevels() |>
   select(player_name, player_id, season, from, until, days_lost,
          games_lost, injury, injury_acl, injury_type, injury_severity)
 
 ## correct/delete wrongly recorded 'transfermarkt' injuries:
 ## directly delete problematic players
 problem_players <- c("arjen-robben", "james-rodriguez", "philippe-coutinho", "thibaut-courtois")
-raw_df_injuries <- raw_df_injuries %>%
+raw_df_injuries <- raw_df_injuries |>
   filter(!(player_name %in% problem_players))
 ##
 
